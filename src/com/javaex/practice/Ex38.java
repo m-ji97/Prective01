03145a8f1dda3c217e907d3a3793fbb6fd44e57a
m-ji97 <tv5035@naver.com>
2023-12-26 19:00:22 +0900

    "."

--- src/com/javaex/practice/Ex38.java
+++ src/com/javaex/practice/Ex38.java
@@ -6,15 +6,14 @@ public class Ex38 {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("전체금액을 입력해주세요: ");
-		int a = sc.nextInt(); //int a변수에 전체금액이 들어감
 		
-		System.out.print(a);
+		System.out.print("전체금액을 입력해주세요: ");
+		int ply = sc.nextInt(); //int a변수에 전체금액이 들어감
 		
 		System.out.println("실제지불금액은");
 		
-		int b = (a / 100) * 100;
-		System.out.println(b+"입니다");
+		int ply2 = (ply / 1000) * 1000;
+		System.out.println(ply2+"입니다");
 		sc.close();
 	}
 
